0d1ee3247d5d90870efaeffd68bbc2118cdfb925
rahulch-1 <105146716+rahulch-1@users.noreply.github.com>
2023-07-29 11:56:26 +0530

    Update login.js final commit backing

--- login.js
+++ login.js
@@ -1,34 +1,34 @@
 function validateLogin() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-  
-    // Replace the following dataset with your Excel data once you have a server-side mechanism to fetch the data.
-    const userData = [
-      { username: "user1", password: "password1" },
-      { username: "user2", password: "password2" },
-      { username: "user3", password: "password3" },
-      // Add more users as needed
-    ];
-   const foundUser = userData.find((user) => user.username === username);
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Replace the following dataset with your Excel data once you have a server-side mechanism to fetch the data.
+  const userData = [
+    { username: "user1", password: "password1" },
+    { username: "user2", password: "password2" },
+    { username: "user3", password: "password3" },
+    // Add more users as needed
+  ];
+
+  const foundUser = userData.find((user) => user.username === username);
   const foundPass = userData.find((user) => user.password === password);
 
   if (foundUser && foundPass) {
     document.getElementById("loginMessage").innerText = "Login successful!";
     return true;
-  } else if (foundUser && !foundPass) {
-    // document.getElementById("loginMessage").innerText = "Invalid username or password!";
-    document.getElementById("loginMessage").innerText =
-      "Username is valid \n Invalid password!";
+  }
+  // } else if (foundUser && !foundPass) {
+  //   // document.getElementById("loginMessage").innerText = "Invalid username or password!";
+  //   document.getElementById("loginMessage").innerText =
+  //     "Username is valid \n Invalid password!";
 
-    return false;
-  } else if (foundPass && !foundUser) {
-    document.getElementById("loginMessage").innerText =
-      "Password valid \n Invalid username!";
-    return false;
-  } else {
-    document.getElementById("loginMessage").innerText =
-      "Invalid username or password!";
-    return false;
-  }
-  }
-  
+  //   return false;
+  // } else if (foundPass && !foundUser) {
+  //   document.getElementById("loginMessage").innerText =
+  //     "Password valid \n Invalid username!";
+  //   return false;
+  // } else {
+  document.getElementById("loginMessage").innerText =
+    "Invalid username or password!";
+  return false;
+}
